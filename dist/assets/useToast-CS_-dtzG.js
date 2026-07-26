c06eb3c705eb2287d1f6d53271d4039da727f3b9
bendoujanna <b.vitalina@alustudent.com>
2026-07-27 01:51:11 +0200

    Stop tracking dist folder to protect secrets

--- dist/assets/useToast-CS_-dtzG.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{c as t}from"./vendor-map-DaiWH_Rq.js";import{v as n}from"./index-DMFiJbsb.js";var r=e(t(),1);function i(){return(0,r.useContext)(n)}export{i as t};
